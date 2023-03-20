7079ba1b1eb8b7e0f4863f9e2d94991d52d93312
aarontigasin <aaronzuluetaofficial@gmail.com>
2023-03-20 17:12:02 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log('Hello, Welcome to De La Salle University');
+
+let firstName = 'Jenny'
+let lastName = 'Kim'
+let age = 25
+let hobbies = ['Playing Guitar', 'Dancing', 'Singing', 'Writing Poems', 'Reading']
+let homeAddress = {
+		houseNumber: 18,
+		street: 'Bagong Silang',
+		barangay: 'Salawag',
+		city: 'Dasmarinas',
+		province: 'Cavite',
+		zipCode: 4114,
+	}	
+	
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(hobbies)
+console.log(homeAddress)
